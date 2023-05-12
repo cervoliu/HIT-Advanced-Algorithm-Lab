ffbe34e1fa3b066bc4b4ecd946413b3d03299e95
cervoliu <cervol@foxmail.com>
2023-05-12 15:03:36 +0800

    fix file io warning

--- lab3/checker.cpp
+++ lab3/checker.cpp
@@ -4,7 +4,19 @@ using namespace std;
 
 int main()
 {
-    FILE *f1 = fopen("naive.out", "r"), *f2 = fopen("bloomFilter.out", "r");
+    FILE *f1 = fopen("unordered_multiset.out", "r");
+    if(f1 == NULL) f1 = fopen("naive.out", "r");
+    if(f1 == NULL)
+    {
+        puts("standard output not exist");
+        return 1;
+    }
+    FILE *f2 = fopen("bloomFilter.out", "r");
+    if(f2 == NULL)
+    {
+        puts("test(bloomFilter) output not exist");
+        return 2;
+    }
     int diff = 0, total = 0;
     for(int x, y; fscanf(f1, "%d", &x) != EOF && fscanf(f2, "%d", &y) != EOF; ++total)
     {
--- lab3/gen.cpp
+++ lab3/gen.cpp
@@ -5,15 +5,15 @@ int Random(int l, int r) { return rnd.next(l, r); }
 int main(int argc, char **argv)
 {
     registerGen(argc, argv, 1);
-    freopen("bloomFilter.in", "w", stdout);
+    FILE *fout = fopen("bloomFilter.in", "w");
     int q = 1000000;
     while(q--)
     {
         int type = Random(0, 2);
         if(type == 1) type = 2;
         int x = Random(1, 500000);
-        printf("%d %d\n", type, x);
+        fprintf(fout, "%d %d\n", type, x);
     }
-    fclose(stdout);
+    fclose(fout);
     return 0;
 }
