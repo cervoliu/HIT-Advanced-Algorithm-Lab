5d52422d6126ae8912b1fb09d0e4d1092e0bd4d5
cervoliu <cervol@foxmail.com>
2023-06-14 23:24:34 +0800

    first commit on lab6

---
+++ lab6/analyzer.cpp
@@ -0,0 +1,74 @@
+#include "sketch.h"
+
+#include <iostream>
+#include <pcap/pcap.h>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *buffer);
+
+int main(int argc, char **argv)
+{
+    // Check command-line arguments
+    if (argc < 2) {
+        cerr << "Usage: " << argv[0] << " <interface>" << endl;
+        return 1;
+    }
+
+    // Open network interface for packet capture
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t *handle = pcap_open_live(argv[1], BUFSIZ, 1, 1000, errbuf);
+    if (handle == NULL) {
+        cerr << "Error: could not open network interface " << argv[1] << ": " << errbuf << endl;
+        return 1;
+    }
+    // Initialize Sketch data structure
+    CM_Sketch sketch(1000000, 4);
+
+    // Start packet capture loop
+    pcap_loop(handle, 1000, process_packet, (u_char *)&sketch);
+
+    // Close network interface
+    pcap_close(handle);
+
+    for(int i = 0; i < 200; ++i)
+        printf("%d\n", sketch.query(i));
+    return 0;
+}
+
+void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *buffer)
+{
+    // Extract Sketch data structure from arguments
+    auto *sketch = (Sketch *)args;
+
+    // Parse Ethernet header
+    const u_char *ethernet_header = buffer;
+    int ethernet_type = (ethernet_header[12] << 8) | ethernet_header[13];
+    if (ethernet_type != 0x0800) {
+        return; // Not an IP packet
+    }
+
+    // Parse IP header
+    const u_char *ip_header = buffer + 14;
+    int ip_length = (ip_header[0] & 0x0f) * 4;
+    const u_char *ip_payload = ip_header + ip_length;
+    int ip_protocol = ip_header[9];
+    if (ip_protocol != 6) {
+        return; // Not a TCP packet
+    }
+
+    // Parse TCP header
+    const u_char *tcp_header = ip_payload;
+    int tcp_length = ((tcp_header[12] >> 4) & 0x0f) * 4;
+    const u_char *tcp_payload = tcp_header + tcp_length;
+
+    // Extract source IP address and payload length
+    string src_ip = to_string(ip_header[12]) + "." + to_string(ip_header[13]) + "." + to_string(ip_header[14]) + "." + to_string(ip_header[15]);
+    int payload_length = header->len - (ip_length + tcp_length);
+
+    // Update Sketch with payload length for source IP address
+    sketch->insert(payload_length);
+    //printf("%d\n", sketch->query(payload_length));
+}
---
+++ lab6/sketch.h
@@ -0,0 +1,130 @@
+#include <vector>
+#include <random>
+#include <algorithm>
+
+using namespace std;
+
+typedef pair<int, int> PII;
+
+const int INF = 1e9;
+const int mo = 1e9 + 7; // must be a prime
+
+int random(int l, int r) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(l, r);
+    return dis(gen);
+}
+
+class Sketch
+{
+protected:
+    int n; // the size of hash table
+    vector<int> tbl; // the hash table
+    int m; // the number of hash functions, such as h(x) = (px + q) mod mo mod n
+    vector<PII> h;
+    int hash(int x, PII p)
+    {
+        return (1LL * p.first * x % mo + p.second) % mo % n;
+    }
+public:
+    Sketch(int _n, int _m) : n(_n), m(_m)
+    {
+        tbl.resize(n);
+        for(int i = 0; i < m; ++i)
+        {
+            h.push_back(make_pair(random(1, mo - 1), random(0, mo - 1)));
+        }
+    }
+    virtual void insert(int x) = 0; //pure virtual function, to be inherited
+    virtual int query(int x) = 0;
+};
+
+class CM_Sketch : public Sketch
+{
+    using Sketch::Sketch; // implicitly inherite constructer
+public:
+    void insert(int x)
+    {
+        for(auto p : h)
+        {
+            int pos = hash(x, p);
+            tbl[pos]++;
+        }
+    }
+    int query(int x)
+    {
+        int res = INF;
+        for(auto p : h)
+        {
+            int pos = hash(x, p);
+            res = min(res, tbl[pos]);
+        }
+        return res;
+    }
+};
+
+class CU_Sketch : public Sketch
+{
+    using Sketch::Sketch;
+public:
+    void insert(int x)
+    {
+        static vector<int> pos_vec;
+        pos_vec.clear();
+        int min_val = 0;
+        for(auto p : h)
+        {
+            int pos = hash(x, p);
+            pos_vec.push_back(pos);
+            min_val = min(min_val, tbl[pos]);
+        }
+        for(auto pos : pos_vec)
+        {
+            if(tbl[pos] == min_val) tbl[pos]++;
+        }
+    }
+    int query(int x)
+    {
+        int res = INF;
+        for(auto p : h)
+        {
+            int pos = hash(x, p);
+            res = min(res, tbl[pos]);
+        }
+        return res;
+    }
+};
+
+class Count_Sketch : public Sketch
+{
+    using Sketch::Sketch;
+public:
+    int hash(int x, PII p, int &sig)
+    {
+        int val = (1LL * p.first * x % mo + p.second) % mo;
+        sig = (val & 1) ? 1 : -1;
+        return val % n;
+    }
+    void insert(int x)
+    {
+        for(auto p : h)
+        {
+            int sig, pos = hash(x, p, sig);
+            tbl[pos] += sig;
+        }
+    }
+    int query(int x)
+    {
+        static vector<int> cnt_vec;
+        cnt_vec.clear();
+        for(auto p : h)
+        {
+            int sig, pos = hash(x, p, sig);
+            cnt_vec.push_back(sig * tbl[pos]);
+        }
+        sort(cnt_vec.begin(), cnt_vec.end());
+        if(m & 1) return cnt_vec[m / 2 + 1];
+        else return (cnt_vec[m / 2] + cnt_vec[m / 2 + 1]) / 2;
+    }
+};
